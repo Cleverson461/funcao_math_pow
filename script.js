352cde86c1661e041ed85b14310d8008f9711d3b
cleverson <cleverson461@gmail.com>
2024-03-14 22:55:04 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function pontencia(n1, n2){
+  let result = Math.pow(n1, n2);
+
+  return result
+}
+
+document.write(`A potencia: ${pontencia(2, 2)}<br/>`)
+document.write(`A potencia: ${pontencia(3, 2)}<br/>`)
+document.write(`A potencia: ${pontencia(18, 2)}`)
